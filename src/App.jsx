3121862ee4bdc6b8678a2b60abc2d647bb4eccfb
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-19 16:12:20 -0300

    first 16-grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import { TURNS } from './constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(16).fill("🟢"))
 
   return (
-    <main>
+    <main className='main'>
+      <h1>4 IN A ROW</h1>
+      <section className='board'>
+        {board.map((_, index) => {
+          return (
+            <div key={index}>
+                <span>{board[index]}</span>
+            </div>
+          )
+        })}
+      </section>
+      <section className='turns'>
+        <div>
+          <span>{TURNS.red}</span>
+        </div>
+        <div>
+          <span>{TURNS.green}</span>
+        </div>
+      </section>
     </main>
   )
 }
