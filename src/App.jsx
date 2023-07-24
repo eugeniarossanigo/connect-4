a31ff12def9116bae7876b9b7716fca009760d84
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-24 19:08:15 -0300

    save storage board and turn

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import { TURNS } from './constants'
 import { checkWinner, checkGame } from './logic/board'
 
 function App() {
-  const [board, setBoard] = useState(Array(49).fill('black'))
-  const [turn, setTurn] = useState(TURNS.red)
+  // const [board, setBoard] = useState(Array(49).fill('black'))
+  const [board, setBoard] = useState(() => {
+    const storageBoard = window.localStorage.getItem('board')
+    return storageBoard ? JSON.parse(storageBoard) : Array(49).fill('black')
+  })
+  // const [turn, setTurn] = useState(TURNS.red)
+  const [turn, setTurn] = useState(() => {
+    const storageTurn = window.localStorage.getItem('turn')
+    return storageTurn ?? TURNS.red
+  })
   const [winner, setWinner] = useState(null)
 
   const handleClick = (index) => {
@@ -20,6 +28,8 @@ function App() {
     setBoard(newBoard)
     const newTurn = turn === TURNS.red ? TURNS.yellow : TURNS.red
     setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
@@ -33,6 +43,8 @@ function App() {
     setBoard(Array(49).fill('black'))
     setWinner(null)
     setTurn(TURNS.red)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
