57c64df1452512c0a8eb0644d8882fdd0435f4db
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-23 21:42:52 -0300

    update logic for 5x5 board

--- src/App.jsx
+++ src/App.jsx
@@ -4,69 +4,52 @@ import { TURNS } from './constants'
 import { checkWinner, checkGame } from './logic/board'
 
 function App() {
-  // const [board, setBoard] = useState(Array(16).fill('black'))
-  const [board, setBoard] = useState(Array(4).fill(Array(4).fill('black')))
+  const [board, setBoard] = useState(Array(25).fill('black'))
   const [turn, setTurn] = useState(TURNS.red)
   const [winner, setWinner] = useState(null)
 
-  const handleClick = (array, indexArray, indexItem) => {
-    updateBoard(array, indexArray, indexItem)
+  const handleClick = (index) => {
+    updateBoard(index)
   }
 
-  const updateBoard = (array, indexArray, indexItem) => {
-    // if (board[index] !== 'black' || winner) return
-    // const newBoard = [...board]
-    // newBoard[index] = turn
-    // setBoard(newBoard)
-    // const newTurn = turn === TURNS.red ? TURNS.green : TURNS.red
-    // setTurn(newTurn)
-
-    // const newWinner = checkWinner(newBoard, board)
-    // if (newWinner) {
-    //   console.log("ganó " + turn)
-    //   setWinner(newWinner)
-    // } else if (checkGame(newBoard)) {
-    //   setWinner(false)
-    // }
-
-    if (array[indexItem] !== 'black' || winner) return
+  const updateBoard = (index) => {
+    let x = index < 20 && board[index + 5] === 'black'
+    console.log(index)
+    console.log(index + 5)
+    if (board[index] !== 'black' || x || winner) return
     const newBoard = [...board]
-    const newArray = [...array]
-    newArray[indexItem] = turn
-    newBoard[indexArray] = newArray
-    console.log(newBoard)
+    newBoard[index] = turn
     setBoard(newBoard)
-    const newTurn = turn === TURNS.red ? TURNS.green : TURNS.red
+    const newTurn = turn === TURNS.red ? TURNS.yellow : TURNS.red
     setTurn(newTurn)
 
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      console.log("ganó " + turn)
+      setWinner(newWinner)
+    } else if (checkGame(newBoard)) {
+      setWinner(false)
+    }
   }
 
   return (
     <main className='main'>
       <h1>4 IN A ROW</h1>
-      <section className='board'>
-        {board.map((array, index) => array.map((_, i) => {
-          return (
-            <div key={i} onClick={() => handleClick(array, index, i)}>
-                <span className={array[i]}></span>
-            </div>
-          )
-         }))}
-        
-        {/* {board.map((_, index) => {
+      <section className='board'>        
+        {board.map((_, index) => {
           return (
             <div key={index} onClick={() => handleClick(index)}>
                 <span className={board[index]}></span>
             </div>
           )
-        })} */}
+        })}
       </section>
       <section className='turns'>
         <div className={`${turn === TURNS.red && 'is-selected'}`}>
           <span className={TURNS.red}></span>
         </div>
-        <div className={`${turn === TURNS.green && 'is-selected'}`}>
-          <span className={TURNS.green}></span>
+        <div className={`${turn === TURNS.yellow && 'is-selected'}`}>
+          <span className={TURNS.yellow}></span>
         </div>
       </section>
     </main>
--- src/logic/board.js
+++ src/logic/board.js
@@ -1,14 +1,14 @@
 import { WINNER_COMBOS } from '../constants';
 
-export const checkWinner = (board, array) => {
-    for (let combo of WINNER_COMBOS(array)) {
-        const [a, b, c, d] = combo
-        if (board[a] !== 'black' && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
-            return board[a]
-        }
+export const checkWinner = (board) => {
+  for (let combo of WINNER_COMBOS) {
+    const [a, b, c, d] = combo
+    if (board[a] !== 'black' && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
+      return board[a]
     }
+  }
 }
 
 export const checkGame = (board) => {
-    return board.every(slot => slot !== 'black')
+  return board.every(slot => slot !== 'black')
 }
