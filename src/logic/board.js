7f51166d8c6bed6baeccbeb1ca08c0a470130b47
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-07-24 16:46:08 -0300

    update checkWinner function

--- src/logic/board.js
+++ src/logic/board.js
@@ -4,7 +4,9 @@ export const checkWinner = (board) => {
   for (let combo of WINNER_COMBOS) {
     const [a, b, c, d] = combo
     if (board[a] !== 'black' && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
-      return board[a]
+      let winner = board[a]
+      combo.map(i => board[i] = 'winner-orange')
+      return winner
     }
   }
 }
